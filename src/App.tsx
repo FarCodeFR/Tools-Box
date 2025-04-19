023aaee6113b45249a674783e545cb9f3bce5684
FarCodeFR <Timo-44@hotmail.com>
2025-04-19 16:54:48 +0200

    fix html

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
     <section className='container-card-qr'>
     <picture>
       <img src="/images/QRC.png" alt="QR code pour accéder au site Front-End Mentor" />
-    </picture>
     <figcaption>
       <h2>Improve your front-end skills by building projects</h2>
       <p>Scan the QR code to visit Frontend <br/> Mentor and take your coding skills to <br/> the next level</p>
     </figcaption>
+    </picture>
     </section>
   )
 }
